4d811cbfaf0dd9deda2788d9b13059d873a402b5
unknown <yuvrajparashar710@gmail.com>
2024-02-27 15:01:53 +0530

    update

--- reduxtoolkittodo/src/features/todo/todoSlice.js
+++ reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -10,11 +10,19 @@ export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        addTodo: ()=>{
-
+        addTodo: (state, action)=>{
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo);
         },
-        reduceTodo: ()=>{
-            
+        removeTodo: (state, action)=>{
+            state.todos = state.todos.filter(todo=>todo.id!==action.payload)
         }
     }
 });
+
+export const {addTodo, removeTodo} = todoSlice.actions;
+
+export default todoSlice.reducer;
